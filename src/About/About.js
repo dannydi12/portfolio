011f74c39e3c780d35a0f2f0ac4ec96ecaca3795
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-18 13:13:01 -0800

    code with avatar but I don't like it

--- src/About/About.js
+++ src/About/About.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './About.css'
 import ScaleText from 'react-scale-text';
+import Avatar from 'avataaars';
 
 function About() {
   return (
     <section className='about-section'>
-      <header>
-        <ScaleText maxFontSize={100} widthOnly={true}>
-          <h2 className='title'>About</h2>
-        </ScaleText>
-      </header>
       <div className='row'>
         <div className='column'>
+          <header>
+            <ScaleText maxFontSize={100} widthOnly={true}>
+              <h2 className='title'>About</h2>
+            </ScaleText>
+          </header>
           <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool. When I'm not online, I can be found writing songs and acting on TV.</p>
+          <p>I speak HTML, CSS, Javascript, and React oh, also English and French</p>
         </div>
-        <p className='column'>I speak HTML, CSS, Javascript, and React
-oh, also English and French</p>
+        <div className='column'>
+          <Avatar
+            avatarStyle='Transparent'
+            topType='ShortHairShortWaved'
+            accessoriesType='Blank'
+            hairColor='Brown'
+            facialHairType='Blank'
+            clotheType='Hoodie'
+            clotheColor='Blue03'
+            eyeType='Happy'
+            eyebrowType='FlatNatural'
+            mouthType='Smile'
+            skinColor='Light'
+          />
+          <p>That's me! Well not really...</p>
+        </div>
       </div>
     </section>
   );
