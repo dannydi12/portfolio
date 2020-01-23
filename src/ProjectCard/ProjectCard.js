521e8e2ce02ecdb2834345180556828933f98eef
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-23 14:52:01 -0800

    reverted to before major project section changes

--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -2,18 +2,21 @@ import React, { useContext } from 'react';
 import './ProjectCard.css';
 import WordList from '../WordList/WordList';
 import widthContext from '../widthContext';
-import ProjectInfo from '../ProjectInfo/ProjectInfo';
 
 function ProjectCard(props) {
   const width = useContext(widthContext);
   return (
     <div className='project-card'>
       <img className='project-image' src={props.project.image} alt={props.project.title + ' landing page'} />
-      {width.isMobile &&
-        <div className='project-wrapper'>
-          <ProjectInfo project={props.project} />
+      <div className='project-wrapper'>
+        <h5 className='project-title'>{props.project.title}</h5>
+        <p>{props.project.description}</p>
+        <WordList words={props.project.languages} />
+        <div className='project-links'>
+          <a className='underline' target='_blank' rel="noopener noreferrer" href={props.project.live}>Live</a>
+          <a className='underline' target='_blank' rel="noopener noreferrer" href={props.project.repo}>Repo</a>
         </div>
-      }
+      </div>
     </div>
   );
 }
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,39 +3,26 @@ import './Projects.css';
 import ScaleText from 'react-scale-text';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import STORE from './ProjectsStore';
-import ProjectInfo from '../ProjectInfo/ProjectInfo';
 
 function Projects() {
   const [projectState, setProjectState] = useState({
-    currentProject: 1,
     currentCategory: 'basic'
   })
 
   //will need to add filter or object rank for categories before map
   let displayProjects = STORE.map((project, i) =>
-    (<ProjectCard
-      key={i}
-      project={project}
-      setProjectState={setProjectState} />)
+    (<ProjectCard key={i} project={project} />)
   );
 
   return (
     <section className='section projects-section'>
-      <div className='row'>
-        <div className='column'>
-          <header>
-            <ScaleText maxFontSize={100} widthOnly={true}>
-              <h2 className='title underline'>Projects</h2>
-            </ScaleText>
-          </header>
-          <b />
-          <div className='project-preview'>
-            <ProjectInfo project={STORE[projectState.currentProject]} />
-          </div>
-        </div>
-        <div className='column'>
-          {displayProjects}
-        </div>
+      <header>
+        <ScaleText maxFontSize={100} widthOnly={true}>
+          <h2 className='title underline'>Projects</h2>
+        </ScaleText>
+      </header>
+      <div className='project-list'>
+        {displayProjects}
       </div>
     </section>
   );
